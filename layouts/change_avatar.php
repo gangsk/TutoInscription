c567bb5674553773a72ea5285b06bb3578e7e5f9
NoS1gnal <poltorvs@gmail.com>
2020-07-21 02:01:25 +0200

    Rewrite code patch failure upload mime, nullByte, double extension

--- layouts/change_avatar.php
+++ layouts/change_avatar.php
@@ -3,26 +3,73 @@
     $dir = "../assets/img";
     $target_file = $dir.basename($_FILES['avatar_file']['name']);
     $imgTypeFile = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
+    $fileMime = $_FILES['avatar_file']['type'];
+    
 
 
-
-    if(check_mime($_FILES['avatar_file']['tmp_name'])){
-        if(check_size($_FILES['avatar_file']['size'])){
-            if(check_type($imgTypeFile)){
-                if(move_uploaded_file($_FILES['avatar_file']['tmp_name'], $target_file)){
-                    echo "Fichier ". basename($_FILES['avatar_file']['name']) . " a bien été uploadé";
+    if (check_mime($_FILES['avatar_file']['tmp_name'])) {
+        if (check_size($_FILES['avatar_file']['size'])) {
+            if (check_type($imgTypeFile)) {
+                if (check_double_extension($_FILES['avatar_file']['name'])) {
+                    if(check_type_mime($fileMime)) {
+                        if(check_null_byte($_FILES['avatar_file']['name'])){
+                            if(move_uploaded_file($_FILES['avatar_file']['tmp_name'], $target_file)){
+                                    echo "Fichier ". basename($_FILES['avatar_file']['name']) . " a bien été uploadé";
+                            }else{
+                                echo "Erreur lors de l'upload ";
+                            }
+                        }else {
+                            echo "ERreur null byte";
+                        }
+                    }else {
+                        echo "Erreur mime";
+                    }
+                } else {
+                    echo "Erreur extension";
                 }
-            }else 
-            {
-                echo "Extension mauvaises";
+            } else {
+                echo "Erreur type ";
             }
-        }else{ 
-            echo "Too big";
+        } else {
+            echo "Trop gros";
         }
-    }else{
-        echo "Fake";
+    } else {
+        echo "Erreur image";
     }
 
+
+    function check_null_byte($file){
+        if(preg_match('%', $file)){
+            return false;
+        }else {
+            return true;
+        }
+    }
+
+
+
+    function check_type_mime($fileMime){
+        $mime = array('image/png', 'image/jpeg', 'image/gif');
+
+        foreach($mime as $item){
+            if($item == $fileMime){
+                return true;
+            }else {
+                return false;
+            }
+        }
+    }
+
+
+    function check_double_extension($file){
+        $extension = explode('.', $file);
+        if($extension[1] == "php"){
+            return false;
+        }
+        else {
+            return true;
+        }
+    }
 
 
     function check_mime($file){
@@ -45,3 +92,6 @@
             return true;
         }
     }
+
+
+
