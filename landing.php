462fdfe0ef13e5b2f60d3d2a68a3c9c4e2b2b76a
Geistnigmareicharbeitungerheit <37757623+NoS1gnalWeb@users.noreply.github.com>
2021-02-26 12:05:52 +0100

    Update landing.php ajout du bouton déconnexion

--- landing.php
+++ landing.php
@@ -38,6 +38,7 @@
                 <div class="text-center">
                         <h1 class="p-5">Bonjour ! <?php echo $_SESSION['user']; ?></h1>
                         <hr />
+                        <a href="deconnexion.php" class="btn btn-danger btn-lg">Déconnexion</a>
                         <!-- Button trigger modal -->
                         <button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#change_password">
                           Changer mon mot de passe
