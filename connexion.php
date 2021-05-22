975fed7b224ba2cfbae1a6076f88c0783e0be5c8
Geistnigmareicharbeitungerheit <37757623+NoS1gnalWeb@users.noreply.github.com>
2021-05-23 01:31:12 +0200

    Update connexion.php

--- connexion.php
+++ connexion.php
@@ -1,24 +1,29 @@
 <?php 
-    session_start();
-    require_once 'config.php';
+    session_start(); // Démarrage de la session
+    require_once 'config.php'; // On inclu la connexion à la base de données
 
-    if(!empty($_POST['email']) && !empty($_POST['password']))
+    if(!empty($_POST['email']) && !empty($_POST['password'])) // Si il existe les champs email, password et qu'il sont pas vident
     {
-        $email = htmlspecialchars($_POST['email']);
+        // Patch XSS
+        $email = htmlspecialchars($_POST['email']); 
         $password = htmlspecialchars($_POST['password']);
 
+        // On regarde si l'utilisateur est inscrit dans la table utilisateurs
         $check = $bdd->prepare('SELECT pseudo, email, password FROM utilisateurs WHERE email = ?');
         $check->execute(array($email));
         $data = $check->fetch();
         $row = $check->rowCount();
 
-        if($row == 1)
+        // Si > à 0 alors l'utilisateur existe
+        if($row > 0)
         {
+            // Si le mail est bon niveau format
             if(filter_var($email, FILTER_VALIDATE_EMAIL))
             {
-                
+                // Si le mot de passe est le bon
                 if(password_verify($password, $data['password']))
                 {
+                    // On créer la session et on redirige sur landing.php
                     $_SESSION['user'] = $data['email'];
                     header('Location: landing.php');
                     die();
