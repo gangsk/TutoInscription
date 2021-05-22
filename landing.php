a69a5e3b72e2d193f559e1e04cbc37afd7100562
Geistnigmareicharbeitungerheit <37757623+NoS1gnalWeb@users.noreply.github.com>
2021-05-23 01:43:08 +0200

    Update landing.php

--- landing.php
+++ landing.php
@@ -1,5 +1,6 @@
 <?php 
     session_start();
+    // si la session existe pas soit si l'on est pas connecté on redirige
     if(!isset($_SESSION['user'])){
         header('Location:index.php');
         die();
