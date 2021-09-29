b994b3e545f009c8256a1bc16e6b25b844158ab1
Geistnigmareicharbeitungerheit <37757623+NoS1gnalWeb@users.noreply.github.com>
2021-09-29 10:04:04 +0200

    Update config.php

--- config.php
+++ config.php
@@ -3,12 +3,12 @@
            Attention ! le host => l'adresse de la base de données et non du site !!
         
            Pour ceux qui doivent spécifier le port ex : 
-           $bdd = new PDO("mysql:host=HOST;dbname=DB_NAME;charset=utf8;port=3306", "LOGIN", "PASS");
+           $bdd = new PDO("mysql:host=CHANGER_HOST_ICI;dbname=CHANGER_DB_NAME;charset=utf8;port=3306", "CHANGER_LOGIN", "CHANGER_PASS");
            
          */
     try 
     {
-        $bdd = new PDO("mysql:host=HOST;dbname=DB_NAME;charset=utf8", "LOGIN", "PASS");
+        $bdd = new PDO("mysql:host=CHANGER_HOST_ICI;dbname=CHANGER_DB_NAME;charset=utf8", "CHANGER_LOGIN", "CHANGER_PASS");
     }
     catch(PDOException $e)
     {
